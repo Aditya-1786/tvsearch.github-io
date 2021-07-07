112d90c32c61a283248c3aeceac3e67c602723a6
Aditya-1786 <71499928+Aditya-1786@users.noreply.github.com>
2021-07-07 18:41:32 +0530

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,10 +1,16 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', async (e)=>{
     e.preventDefault();
-    const search = form.elements.q.value;
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${search}`);
-    const image = res.data[0].show.image.medium;
-    const img  = document.createElement('img');
-    img.src = image;
-    form.append(img);
+    try{
+        const search = form.elements.q.value;
+        const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${search}`);
+        const image = res.data[0].show.image.medium;
+        const img  = document.createElement('img');
+        img.src = image;
+        form.append(img);
+    }
+    catch(e){
+        console.log("ERROR!!!:",e);
+    }
+    
 })

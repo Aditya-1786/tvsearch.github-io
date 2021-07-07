955ac34ba685ba410f6aad7cc965bd7bd88e79ae
Aditya-1786 <71499928+Aditya-1786@users.noreply.github.com>
2021-07-07 18:27:56 +0530

    start

---
+++ tv.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    const search = form.elements.q.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${search}`);
+    const image = res.data[0].show.image.medium;
+    const img  = document.createElement('img');
+    img.src = image;
+    form.append(img);
+})

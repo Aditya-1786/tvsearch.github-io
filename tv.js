b23db3b6f9ee8efa56b314f7d6c76e8ac662b68c
Aditya-1786 <71499928+Aditya-1786@users.noreply.github.com>
2021-07-07 18:44:51 +0530

    Update tv.js

--- tv.js
+++ tv.js
@@ -3,7 +3,7 @@ form.addEventListener('submit', async (e)=>{
     e.preventDefault();
     try{
         const search = form.elements.q.value;
-        const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${search}`);
+        const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${search}`);
         const image = res.data[0].show.image.medium;
         const img  = document.createElement('img');
         img.src = image;
